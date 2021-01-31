fcd0dcb79bfea582c24245409a779fa0a9a7b10d
Yuriy Sirotenko <yosirotenko@gmail.com>
2021-01-31 20:13:22 +0600

    change answers gui

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -28,6 +28,13 @@ class Answer extends React.Component
 			color: "green", paddingBottom: "5px"
 		} : {paddingBottom: "5px"};
 
+		let color = "#b9b9b9";
+		if (this.props.answerSelected == "true") {
+			color = "green";
+		}
+
+		style = Object.assign(style, {display: "inline-block", verticalAlign: "top", width: "100px", overflow: "hidden", border: "1px solid " + color, borderRadius: "3px", marginRight: "3px", marginBottom: "3px", padding: "5px", height: "30px"});
+
 		return (
 			<div style={style}>
 				<span>{this.props.number}. </span>
